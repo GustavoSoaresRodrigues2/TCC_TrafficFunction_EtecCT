8bff60fad7b46d9d248d3d0ff6efcb1f0ec3b189
João V <73762324+JoaoV-A01@users.noreply.github.com>
2022-11-16 17:03:48 -0300

    Update login.php

--- sistemaLogin/login.php
+++ sistemaLogin/login.php
@@ -17,7 +17,7 @@
 	require "conectar.php";
 
 	//Faz uma consulta a tbl_cliente e retorna a linha que contem o usuario digitado
-	$strSQL = "SELECT Nome, Email, Senha FROM dados_usuarios where Email = '$emailDigitado'";
+	$strSQL = "SELECT cod, Nome, Email, Senha FROM dados_usuarios where Email = '$emailDigitado'";
 
 	//Executa a consulta(query) a variavel $consulta contem o resultado da consulta
 	$consulta = mysqli_query($conexao, $strSQL);
@@ -26,10 +26,10 @@
 	//Cada linha vai para um array ($row) usuario mysql_fetch_array
 	//O usuario e senha encontrados no BD são armazenados nas novas variaveis
 	while ($linha = mysqli_fetch_array($consulta)) {
-        $codBD = $linha["cod"];
-        $nomeBD = $linha["Nome"];
-        $cpfBD = $linha["CPF"];
-        $emailBD = $linha["Email"];
+        	$codBD = $linha["cod"];
+        	$nomeBD = $linha["Nome"];
+        	$cpfBD = $linha["CPF"];
+        	$emailBD = $linha["Email"];
 		$senhaBD = $linha["Senha"];
 	}
 
