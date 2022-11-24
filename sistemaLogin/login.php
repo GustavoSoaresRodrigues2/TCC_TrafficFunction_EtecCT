6670bf3a292a7514c0b1c68e94bd3d6a3d381451
João V <73762324+JoaoV-A01@users.noreply.github.com>
2022-11-24 18:43:16 -0300

    Update login.php

--- sistemaLogin/login.php
+++ sistemaLogin/login.php
@@ -14,7 +14,7 @@
 	}
 
 	//Conexão com o Banco de Dados
-	require "conectar.php";
+	require "BDconectar.php";
 
 	//Faz uma consulta a tbl_cliente e retorna a linha que contem o usuario digitado
 	$strSQL = "SELECT cod, Nome, Email, Senha FROM dados_usuarios where Email = '$emailDigitado'";
@@ -28,13 +28,12 @@
 	while ($linha = mysqli_fetch_array($consulta)) {
         	$codBD = $linha["cod"];
         	$nomeBD = $linha["Nome"];
-        	$cpfBD = $linha["CPF"];
         	$emailBD = $linha["Email"];
 		$senhaBD = $linha["Senha"];
 	}
 
 	//Encerra conexão
-	require "desconectar.php";
+	require "BDdesconectar.php";
 
 		//Verifica usuario e senha
 		if ($emailDigitado == $emailBD && $senhaDigitada == $senhaBD) {
@@ -46,155 +45,13 @@
             $_SESSION["emailUser"] = $emailBD;
             $_SESSION["nomeUser"] = $nomeBD;
             
-            $msg_body = "<br><h1 align='center'>Você está logado!</h1>";
-            echo "<br>";
-            $msg_body = $msg_body . "<p align='center'>Podemos começar a iluminar!</p>";
+           header("Location: ../pages/sistemaPerfil/contaUsuario.php");
 		}
 		else {
 			//Se estiver errado fica NO
-			session_start();
+			session_save_path("/tmp"); session_start();
 			$_SESSION["Login"] = "NÃO";
-			$msg_body = "<h1 align='center'>Você NÃO está logado, veja se fez o Cadastro</h1>";
-			$msg_body = $msg_body . "<p align='center'><a href='form_login.html'>Tentar Novamente</a></p>";
+
+            header("Location: form_login.php");
 		}
 ?>
-
-<!DOCTYPE html>
-<html lang="br">
-
-  <head>
-    <title>TF | Login</title>
-    <meta charset="UTF-8">
-    <meta name="author" content="TrafficFunction">
-    <meta name="description" content="Site para divilguar nossa empresa">
-    <meta name="keywords" content="transito, semaforos, html">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <link href="../layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
-  </head>
-
-  <body id="top">
-
-    <!-- Top Background Image Wrapper -->
-    <div class="bgded overlay light" style="background-image:url('../images/backgrounds/background_home.jpeg');"> 
-      <div class="wrapper row0">
-        <div id="topbar" class="hoc clear"> 
-          <div class="fl_left">
-            <ul class="nospace">
-
-              <!-- Contato -->
-              <li><i class="fa fa-phone"></i> +55 (11) xxxxx-xxxx</li>
-              <li><i class="fa fa-envelope-o"></i> trafficfunction@gmail.com</li>
-              <!-- /Contato -->
-
-            </ul>
-          </div>
-          <div class="fl_right">
-          </div>
-        </div>
-      </div>
-      
-      <!-- NavBar -->
-      <div class="wrapper row1">
-        <header id="header" class="hoc clear"> 
-          <div id="logo" class="fl_left">
-
-            <!-- LogoImagem -->
-            <h1 id="logo_top"><a href="../index.html"><img src="../images/logo_trafficfunction.png" alt=""></a></h1>
-            <!-- /LogoImagem -->
-
-          </div>
-          <nav id="mainav" class="fl_right">
-            <ul class="clear">
-              <li><a href="../index.html">Página Inicial</a></li>
-
-              <!-- Parte Guia -->
-              <li><a class="drop">Guias</a>
-                <ul>
-                  <li><a href="quemSomos.html">Quem Somos?</a></li>
-                  <li><a href="mapaInflacoes.html">Mapa de infrações</a></li>
-                  <li><a href="../pages/sistemaComentar/form_comentario.php">Comentários</a></li>
-                </ul>
-              </li>
-              <!-- /Parte Guia -->
-
-              <!-- Parte Cadastro -->
-              <li><a class="drop">Conta</a>
-                <ul>
-                  <li><a href="../pages/sistemaPerfil/contaUsuario.php">Perfil</a></li>
-                  <li><a href="form_login.html">Login</a></li>
-                  <li><a href="form_cadastro.html">Cadastrar</a></li>
-                  <li><a href="logoff.php">Sair</a></li>
-                </ul>
-              </li>
-              <!-- /Parte Cadastro -->
-              
-            </ul>
-          </nav>
-        </header>
-      </div>
-      <!-- /NavBar -->
-      <div id="breadcrumb" class="hoc clear"> 
-      </div>
-    </div>
-    <!-- End Top Background Image Wrapper -->
-
-    <!-- TRECHO DE PHP QUE MOSTRA A MENSAGEM -->
-    <?php
-      echo $msg_body;
-    ?>
-    <!-- /Fim da mensagem -->
-
-    <!-- FOOTER -->
-    <div class="wrapper row4">
-      <footer id="footer" class="hoc clear"> 
-        <article class="one_third first">
-          <h6 class="heading">Motivação</h6>
-          <p>Não é de hoje que encontramos problemas no transito que atrapalham nosso dia a dia.</p>
-          <p>Diversas irregularidades de trânsito (incluindo semáforos, placas danificadas &hellip;</p>
-          <p class="nospace"><a href="../pages/quemSomos.html">Ler mais</a></p>
-        </article>
-        <div class="one_third">
-          <h6 class="heading">Nossos Contatos</h6>
-          <ul class="nospace btmspace-30 linklist contact">
-            <li><i class="fa fa-map-marker"></i>
-              <address>
-              Rua &amp; nº, São Paulo, Cidade Tiradentes
-              </address>
-            </li>
-            <li><i class="fa fa-phone"></i> +55 (11) xxxxx-xxxx</li>
-            <li><i class="fa fa-envelope-o"></i> trafficfunction@gmail.com</li>
-          </ul>
-          <ul class="faico clear">
-            <li><a class="faicon-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-            <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-            <li><a class="faicon-dribble" href="#"><i class="fa fa-dribbble"></i></a></li>
-            <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-          </ul>
-        </div>
-        <div class="one_quarter">
-          <h6 class="heading">Nossos serviços</h6>
-          <ul class="nospace linklist">
-            <li><a href="#" download>App Mobile</a></li>
-            <li><a href="../pages/mapaInflacoes.html">Mapa</a></li>
-            <li><a href="../pages/sistemaComentar/form_comentario.php">Comentarios</a></li>
-            <li><a href="../pages/quemSomos.html">Nossa Empresa</a></li>
-          </ul>
-        </div>
-      </footer>
-    </div>
-    <!-- /FOOTER -->
-
-    <div class="wrapper row5">
-      <div id="copyright" class="hoc clear"> 
-        <p class="fl_left">Copyright &copy; 2022 - Todos os Direitos Reservados a TrafficFunciton<a href="#"></a></p>
-      </div>
-    </div>
-
-    <a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
-
-    <!-- JAVASCRIPTS -->
-    <script src="../layout/scripts/jquery.min.js"></script>
-    <script src="../layout/scripts/jquery.backtotop.js"></script>
-    <script src="../layout/scripts/jquery.mobilemenu.js"></script>
-  </body>
-</html>
